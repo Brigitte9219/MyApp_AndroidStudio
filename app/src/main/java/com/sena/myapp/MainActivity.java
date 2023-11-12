b9ccb38483674cce10b56efba270b66247873e4c
brigitte <mbpadilla4@misena.edu.co>
2023-11-12 09:53:24 -0500

    Se realiza la lógica para que pase de la página principal a la segunda pagina

--- app/src/main/java/com/sena/myapp/MainActivity.java
+++ app/src/main/java/com/sena/myapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.sena.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnDos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnDos=findViewById(R.id.btnCambiar);
+        btnDos.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this, DosActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
